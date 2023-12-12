bbc9d7163b38e13501f3fe38b5f9420fea6ed066
RomaProger1 <101552538+RomaProger1@users.noreply.github.com>
2023-12-12 13:00:10 +0700

    Выполнил практическое задание 1

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ app = FastAPI()
 
 @app.get('/')
 async def f_index():
-    return {"message": "Hello"}
+    return {"message": "Денисов Роман Дмитриевич"}
+@app.get('/users')
+async def f_index():
+    return {"message": "users", "key": "79238282828"}
 
 @app.get('/tools')
 async def f_index():
-    return {"message": "Tools", "key": "HI!"}
+    return {"message": "tools", "key": "junior"}
 
